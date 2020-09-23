e1951297f707eb52390c2eeaf63a3fad3d62d0f6
Lucase <lukasz.marcinkowski@gmail.com>
2020-09-24 01:55:49 +0200

    internationalization with pl and en added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def default_url_options(options={})
+    { :locale => I18n.locale == I18n.default_locale ? nil : I18n.locale  }
+  end
 end
