7b212d6ae6ea197cfa4bc42cf0b3b9bb2984745e
Lucase <lukasz.marcinkowski@gmail.com>
2020-09-23 23:21:02 +0200

    articles, categories, comments scaffolds added

---
+++ db/migrate/20200923211749_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.text :body
+      t.references :category, null: false, foreign_key: true
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
